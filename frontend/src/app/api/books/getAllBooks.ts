a4f87e719066098c81666ec9a59bcd00cf360446
DIMas821k <dmitrij.zubckov2014@yandex.ru>
2025-06-20 23:29:10 +0300

    Create getAllBooks.ts

---
+++ frontend/src/app/api/books/getAllBooks.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosError } from 'axios';
+import { showAlert } from '@/app/alerts';
+import { API_URL } from '../url';
+
+export async function getAllBooks() {
+  try {
+    const token = document.cookie
+      .split('; ')
+      .find(row => row.startsWith('token='))
+      ?.split('=')[1];
+
+    const response = await axios.get(`${API_URL}/books/all`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+
+    if (axiosError.response) {
+      const errorData = axiosError.response.data as { error?: string; message?: string };
+      showAlert(errorData.message || errorData.error || 'Произошла ошибка при загрузке книг', 'error');
+    } else if (axiosError.request) {
+      showAlert('Сервер не отвечает. Проверьте интернет-соединение', 'error');
+    } else {
+      showAlert('Ошибка при отправке запроса', 'error');
+    }
+
+    throw error;
+  }
+}
