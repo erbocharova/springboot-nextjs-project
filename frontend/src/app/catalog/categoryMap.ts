299d27d34c427b4a3914ee2f19b659307a53ab4e
DIMas821k <dmitrij.zubckov2014@yandex.ru>
2025-06-23 20:35:17 +0300

    замена слов

---
+++ frontend/src/app/catalog/categoryMap.ts
@@ -0,0 +1,12 @@
+const categoryMap: Record<string, string> = {
+  FANTASY: 'Фантастика',
+  SCHOOL: 'Школа',
+  HISTORY: 'История',
+  children: 'Детские книги',
+  fantasy: 'Фэнтези',
+  business: 'Бизнес',
+  DETECTIVE:'Детективы',
+  // добавь остальные нужные категории
+}
+
+export default categoryMap
